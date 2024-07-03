548750801da34a631a796d386352f5d7a4daaf71
vikalp-tomer <vikalptomar15@gmail.com>
2024-07-03 11:44:49 +0530

    bring in shadcn and revalidate after adding items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { database } from "@/db/database";
+import { bids as bidsSchema } from "@/db/schema";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
-  return <div>hi there</div>;
+export default async function Home() {
+  const bids = await database.query.bids.findMany();
+  return (
+    <main className="container mx-auto py-12">
+      <form
+        action={async () => {
+          "use server";
+          await database.insert(bidsSchema).values({});
+          revalidatePath("/");
+        }}
+      >
+        <Input name="bid" placeholder="Bid" />
+        <Button>Place Bid</Button>
+        {bids.map((bid) => (
+          <div key={bid.id}>{bid.id}</div>
+        ))}
+      </form>
+    </main>
+  );
 }
